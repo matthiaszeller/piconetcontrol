9d23b124dde6d61edf7b599968bfc7151a0fe174
Matthias Zeller <m.zeller1901@gmail.com>
2024-07-10 11:21:49 +0200

    Release v0.2.1

--- piconetcontrol/__init__.py
+++ piconetcontrol/__init__.py
@@ -2,6 +2,6 @@
 
 __author__ = """Matthias Gilles Zeller"""
 __author_email__ = "matthias.gilles.zeller@gmail.com"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __description__ = "Client-server package to remotely control a Raspberrypi Pi Pico W."
 __url__ = "https://github.com/matthiaszeller/picoserver"

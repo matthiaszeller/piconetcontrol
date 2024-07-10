f5a7f6fcae8c2d09c492e3a50159b4866f949292
Matthias Zeller <m.zeller1901@gmail.com>
2024-07-10 02:04:23 +0200

    Release v0.2.0

--- piconetcontrol/__init__.py
+++ piconetcontrol/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Matthias Gilles Zeller"""
 __author_email__ = "matthias.gilles.zeller@gmail.com"
-__version__ = "0.1.2"
+__version__ = "0.2.0"
 __description__ = "Client-server package to remotely control a Raspberrypi Pi."
 __long_description__ = """This packages provides two classes (`Client` and `Server`)
 to manage a Raspberry Pi board (acting as server) via Wifi.
